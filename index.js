111521513c9942cc6c3660784abfc893b31c3280
Zac Baston <zbaston@gmail.com>
2016-11-07 20:01:32 +0000

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,40 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+const issuesWithUpdatedApiUrl = issues.map((issue) => {
+    return Object.assign({}, issue, {
+        url: issue.url.replace('api.github.com', 'api-v2.github.com')
+    });
+});
+
+const commentCountAcrossIssues = issues.map((issue) => {
+    return issue.comments_count;
+}).reduce((total, comments) => {
+    return total + comments;
+}, 0)
+
+const openIssues = issues.reduce((openIssues, issue) => {
+    if (issue.state == 'open') {
+        openIssues.push(issue)
+    }
+
+    return openIssues
+}, [])
+
+const nonAutomaticIssues = issues.reduce((automaticIssues, issue) => {
+    if (issue.body !== 'This pull request has been automatically created by learn.co.') {
+        automaticIssues.push(issue);
+    }
+
+    return automaticIssues;
+}, [])
+
+document.getElementById('results').innerHTML = nonAutomaticIssues.map(issue => 
+    `
+    <tr>
+        <td>${issue.body}</td>
+        <td>${issue.created_at}</td>
+        <td>${issue.state}</td>
+    </tr>
+    `).join('');
